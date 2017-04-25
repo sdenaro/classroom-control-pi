e2fd5419146a6fa5e2530b256f3c305265c862ec
broizman <brian.roizman@ny.frb.org>
2017-04-25 14:04:11 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  
+  notify { "Hello world! I am ${::fqdn}": }
+
+}
   # example code for the classroom
   include examples::puppetize
   
