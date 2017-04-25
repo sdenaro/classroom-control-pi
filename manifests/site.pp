d69c30c685f4bceda5a5f8a80eae7e81be8712ab
arakoski <arakoski@gmail.com>
2017-04-25 15:53:46 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,4 +30,5 @@ node default {
   include examples::puppetize
   
   notify { "This is the default message from the production environment": }
+  notify { "This is a test for manifest excercise": }
 }
