79a636381219651f40bffec133e5e45cad5eb85d
Judy Xiao <judyxiaox@yahoo.com>
2017-04-26 15:56:21 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,12 +1,15 @@
-class skeleton {
-  file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-  
-  # add a resource to manage /etc/skel/.bashrc
-
-
-}
+ class skeleton { 
+   file { '/etc/skel': 
+    ensure => directory, 
+    owner => 'root', 
+    group => 'root', 
+    mode => '0755', 
+   } 
+   file { '/etc/skel/.bashrc': 
+    ensure => file, 
+    owner => 'root', 
+    group => 'root', 
+    mode => '0644', 
+    source => 'puppet:///modules/skeleton/bashrc', 
+   } 
+ } 
