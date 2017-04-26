680b80de26be43927a0265c4163dbeda5f950b60
rmanalo732 <manalo.roderick@gmail.com>
2017-04-26 15:27:39 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -7,6 +7,12 @@ class skeleton {
   }
   
   # add a resource to manage /etc/skel/.bashrc
-
+file { '/etc/skel/.bashrc':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/skeleton/bashrc',
+}
 
 }
