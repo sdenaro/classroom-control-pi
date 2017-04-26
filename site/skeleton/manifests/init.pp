28b87076da60e7d6027e2f4e2d0b4cca6c545cfe
Chris Kreussling (Flatbush Gardener) <xrisfg@gmail.com>
2017-04-26 15:22:03 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,7 @@
 class skeleton {
   file { '/etc/skel':
 #    ensure => ???,   # what value should go here?
+    ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
