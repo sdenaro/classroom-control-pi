dc6ec6f8da796c3631813dcc5e143443f4d6bbaa
dlevan <dlevan@users.noreply.github.com>
2017-04-27 21:49:48 +0000

    added path

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,7 +36,8 @@ node default {
 node 'dlevan.puppetlabs.vm' {
   notify { "Print this on linux nodes": }
   exec {"cowsay 'Welcome to ${::fqdn}!!!' > /etc/motd":
-    creates => '/etc/motd'
+    creates => '/etc/motd',
+    path    => ['/usr/local/bin'],
   }
 }
 
