b3deb421d345a257bc351e7b996870969e362dca
dlevan <dlevan@users.noreply.github.com>
2017-04-27 02:31:53 +0000

    added bash defaults

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,6 @@
 class skeleton {
   file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
+    ensure => directory,   # what value should go here?
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
@@ -8,5 +8,11 @@ class skeleton {
   
   # add a resource to manage /etc/skel/.bashrc
 
-
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/skeleton/bash-default',
+  }
 }
