e6fed3a5436411a933a94fe8898107750f69cbbb
dlevan <dlevan@users.noreply.github.com>
2017-04-27 20:28:54 +0000

    redis node role is now configurable

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,11 +1,20 @@
-class profile::redis {
+class profile::redis (
+  $master = false
+) {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
   require profile::epel
-  
-  class { 'redis':
-    # what parameter should we pass to set maxmemory to 10mb?
-    
+
+  if $master {
+    $slaveof = undef
   }
-  
+  else {
+    $slaveof = 'master.puppetlabs.vm 6479'
+  }
+
+  class { 'redis':
+    maxmemory => '10mb',
+    bind      => $ipaddres,
+    slaveof   => $slaveof,
+  }
 }
