93226e15ef1a18304c82b29673cba1a51781eef3
rmanalo732 <manalo.roderick@gmail.com>
2017-04-27 10:44:04 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -35,4 +35,8 @@ node default {
 
 node rmanalo732.puppetlabs.vm {
 notify { "This will only be enforced on the Linux container.": }
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+path => '/usr/bin:/usr/local/bin',
+creates => '/etc/motd',
+}
 }
