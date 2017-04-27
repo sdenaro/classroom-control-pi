dad3be00deb2da6cc07275b07a7e8ef7db90d9a7
rmanalo732 <manalo.roderick@gmail.com>
2017-04-27 14:38:21 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -35,8 +35,6 @@ node default {
 
 node rmanalo732.puppetlabs.vm {
 notify { "This will only be enforced on the Linux container.": }
-exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-path => '/usr/bin:/usr/local/bin',
-creates => '/etc/motd',
+notify { "The primary disk is ${::disks['sda']['size']} in size.": }
 }
 }
